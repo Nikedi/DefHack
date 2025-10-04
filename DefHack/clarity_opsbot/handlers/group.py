e92a223fb19d5c4c99023eafbcabe0787d4f62b6
Niko Kataja <niko.kataja@yahoo.com>
2025-10-05 00:14:56 +0300

    minor fix

--- DefHack/clarity_opsbot/handlers/group.py
+++ DefHack/clarity_opsbot/handlers/group.py
@@ -239,7 +239,6 @@ def create_group_handlers(
                 observed_at=msg.date,
                 unit=unit_for_map,
                 observer=observer_for_map,
-                source_type=source_type_for_map,
                 confidence=confidence,
                 accuracy_m=accuracy_val,
                 tags=tags,
